67cb99384f303e069de298d1595944b0c18050cb
Abhinav <abhinavtk97@gmail.com>
2016-03-06 15:35:48 +0530

    "Events & Programmes" (RegistrationMain Activity) gives an fc

--- app/src/main/java/com/saulmm/material/myapplication/RegistrationMainActivity.java
+++ app/src/main/java/com/saulmm/material/myapplication/RegistrationMainActivity.java
@@ -1,12 +1,21 @@
 package com.saulmm.material.myapplication;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.transition.Slide;
 import android.view.Gravity;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.saulmm.material.R;
+
+import net.glxn.qrgen.QRCode;
+import net.glxn.qrgen.image.ImageType;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 public class RegistrationMainActivity extends Activity {
 
@@ -23,6 +32,19 @@ public class RegistrationMainActivity extends Activity {
         Slide slideExitTransition = new Slide(Gravity.BOTTOM);
         slideExitTransition.excludeTarget(android.R.id.navigationBarBackground, true);
         slideExitTransition.excludeTarget(android.R.id.statusBarBackground, true);
+
+        File f = QRCode.from("https://github.com/kenglxn/QRGen").to(ImageType.JPG).file();
+
+        Bitmap myBitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+
+        ImageView myImage = (ImageView) findViewById(R.id.imageView1);
+        myImage.setImageBitmap(myBitmap);
+
+
+
+
+
+
 
     }
 }
